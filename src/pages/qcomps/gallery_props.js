b5c1612677e829d1f77ad1d0189b6d3dd5cdd7a9
koolgax99 <nihar.sanda@gmail.com>
2024-02-07 18:14:29 -0500

    finished the class activities

--- src/pages/qcomps/gallery_props.js
+++ src/pages/qcomps/gallery_props.js
@@ -1,31 +1,45 @@
+function Person({scientist}) {
+  return (
+    <section className="profile">
+      <h2>{scientist.name}</h2>
+      <img
+        className="avatar"
+        src={scientist.link}
+        alt={scientist.name}
+        width={70}
+        height={70}
+      />
+      <ul>
+        <li>
+          <b>Profession: </b>
+          {scientist.profession}
+        </li>
+        <li>
+          <b>Awards: {scientist.noAwards} </b>
+          {scientist.awards}
+        </li>
+        <li>
+          <b>Discovered: </b>
+          {scientist.discovery}
+        </li>
+      </ul>
+    </section>
+  )
+}
+
 export default function Gallery() {
   return (
     <div>
       <h1>Notable Scientists</h1>
-      <section className="profile">
-        <h2>Maria Skłodowska-Curie</h2>
-        <img
-          className="avatar"
-          src='https://i.imgur.com/szV5sdGs.jpg'
-          alt="Maria Skłodowska-Curie"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b>
-            physicist and chemist
-          </li>
-          <li>
-            <b>Awards: 4 </b>
-            (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            polonium (element)
-          </li>
-        </ul>
-      </section>
+      <Person scientist={{
+      name:'Maria Skłodowska-Curie',
+      link:'https://i.imgur.com/szV5sdGs.jpg' ,
+      profession:'physicist and chemist',
+      awards:'Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal',
+      noAwards:'4',
+        discovery:'polonium (element)',
+      }}
+      />
       <section className="profile">
         <h2>Katsuko Saruhashi</h2>
         <img
